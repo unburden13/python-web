88f24aa16b3d596c5821243be21c1c441378dc25
Juan Gonzalez <gonzalez.juandavid13@gmail.com>
2023-04-26 09:56:06 -0500

    Save submitted data to txt and csv

--- web_server/server.py
+++ web_server/server.py
@@ -1,16 +1,43 @@
 from flask import Flask, render_template, request, redirect
+import csv
 
 app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')  # flask automatically looks for files in the 'template' folder
 
 @app.route("/<string:page_name>")
 def my_home(page_name):
     return render_template(page_name)  # flask automatically looks for files in the 'template' folder
 
+def write_to_file(data):
+    with open('./database.txt', 'a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        database.write(f'\n{email}, {subject}, {message}')
+
+def write_to_csv(data):
+    try:
+        with open('./database.csv', 'a', newline='') as database:
+            email = data["email"]
+            subject = data["subject"]
+            message = data["message"]
+            fieldnames = ['email', 'subject', 'message']
+            csv_writer = csv.DictWriter(database, fieldnames=fieldnames, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            if database.tell() == 0:  # if cursor == 0, meaning file is empty
+                csv_writer.writeheader()
+            csv_writer.writerow({'email':email, 'subject':subject, 'message':message})
+    except:
+        return 'something went wrong'
+
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
         data = request.form.to_dict()
-        print(data)
+        write_to_csv(data)
         return redirect('/thankyou.html')
     else:
         return 'something went wrong'
+
